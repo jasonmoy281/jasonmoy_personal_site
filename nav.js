b3a976e994ca58d453ca7b8f727c7d2ae73e6adc
Jason Moy <jasonmoy28@gmail.com>
2020-11-07 22:28:43 +0800

    type: <feat> action: <added qr code for wechat>

--- nav.js
+++ nav.js
@@ -2,16 +2,24 @@ const navIconMobile = document.querySelector(".nav-icon-mobile");
 const navBar = document.querySelector(".nav-bar");
 const navBarText = document.querySelectorAll(".nav-bar li");
 const nav = document.querySelector("nav");
+const wechatIcon = document.querySelector(".wechat");
+const qrCode = document.querySelector(".qr-code");
 
 const navSlide = () => {
   navIconMobile.addEventListener("click", function () {
     navBar.classList.toggle("nav-bar-active");
-    navBarText.remo;
     for (text of navBarText) {
       text.classList.toggle("nav-bar-text-active");
     }
   });
 };
 
-console.log(navBarText);
+const qrCodeShow = () => {
+  wechatIcon.addEventListener("click", function () {
+    qrCode.classList.toggle("qr-code-active");
+  });
+};
+
 navSlide();
+qrCodeShow();
+console.log(qrCode.style);
